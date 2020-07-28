73173d8e5b0927d50e899982f88ea45a6e0dec2f
Chris <chrisramer.97@gmail.com>
2020-07-28 15:18:43 -0700

    Add math that I am very proud of! Went above and beyond to get comfortable with JS!

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,90 @@ const add = function (number1, number2) {
 	return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number"));
-const number2 = parseInt(prompt("Enter another number"));
+const subtract = function(number1, number2) {
+	return number1 - number2;
+}
 
-alert(add(number1, number2));
+const multiply = function(number1, number2) {
+	return number1 * number2;
+}
+
+const divide = function(number1, number2) {
+	return number1 / number2;
+}
+
+const remainder = function(number1, number2) {
+	return number1 % number2;
+}
+
+doMath();
+
+function doMath()
+{
+	const action = prompt("Which math operation would you like to perform?").toLowerCase();
+
+	if (action == "add" || action == "addition") {
+		const number1 = parseInt(prompt("What number would you like to start with?"));
+		const number2 = parseInt(prompt("What number are you adding to " + number1 + "?"));
+		const sum = add(number1, number2);
+		alert(number1 + " + " + number2 + " = " + sum + "\nYou're now an addition wizard!");
+
+		const doContinue = confirm("Would you like to do more math?");
+		if (doContinue) {
+			doMath()
+		}
+	}
+
+	else if (action == "subtract" || action == "subtraction") {
+		const number1 = parseInt(prompt("What number would you like to start with?"));
+		const number2 = parseInt(prompt("What number are you subtracting " + number1 + " from?"));
+		const difference = subtract(number1, number2);
+		alert(number1 + " - " + number2 + " = " + difference + "\nYou're now a subtraction wizard!");
+
+		const doContinue = confirm("Would you like to do more math?");
+		if (doContinue) {
+			doMath()
+		}
+	}
+
+	else if (action == "multiply" || action == "multiplication") {
+		const number1 = parseInt(prompt("What number would you like to start with?"));
+		const number2 = parseInt(prompt("What number are you multipliying " + number1 + " with?"));
+		const product1 = multiply(number1, number2);
+		alert(number1 + " * " + number2 + " = " + product1 + "\nYou're now a multiplication wizard!");
+
+		const doContinue = confirm("Would you like to do more math?");
+		if (doContinue) {
+			doMath()
+		}
+	}
+
+	else if (action == "divide" || action == "division") {
+		const number1 = parseInt(prompt("What number would you like to start with?"));
+		const number2 = parseInt(prompt("What number are you dividing " + number1 + " with?"));
+		const quotient = divide(number1, number2);
+		alert(number1 + " / " + number2 + " = " + quotient + "\nYou're now a division wizard!");
+
+		const doContinue = confirm("Would you like to do more math?");
+		if (doContinue) {
+			doMath()
+		}
+	}
+
+	else if (action == "remainder") {
+		const number1 = parseInt(prompt("What number would you like to start with?"));
+		const number2 = parseInt(prompt("What number are you dividing " + number1 + " with to get the remainder?"));
+		const result = remainder(number1, number2);
+		alert(number1 + " % " + number2 + " = " + result + "\nYou're now a super division wizard!");
+
+		const doContinue = confirm("Would you like to do more math?");
+		if (doContinue) {
+			doMath()
+		}
+	}
+
+	else {
+		alert(action + " is not a valid math operation! Please specifiy a valid math operation ya goof!");
+		doMath();
+	}
+}
